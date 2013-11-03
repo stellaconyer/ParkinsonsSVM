1c280985c4a36fe83b60496a36ffbcae31c4d1a8
Hackbright Student <stella@stellacotton.com>
2013-11-02 17:49:55 -0700

    Add average for x value in RMS calculation

--- parse_csv.py
+++ parse_csv.py
@@ -1,23 +1,12 @@
 import os
 import sys
 import tarfile
+from datetime import datetime
 
 #Take in a CSV from CLI
 #Choose to write to file or print to standard output
 # -----------
 
-
-# TO APPEND TO CSV
-# my_file = open("data.csv", "ab")
-
-
-# path = r'/csv_files'
-# data = {}
-
-# for dir_entry in os.listdir(path):
-#     dir_entry_path = os.path.join(path, dir_entry)
-#     with open(dir_entry_path, 'r') as my_file:
-#         data[dir_entry] = my_file.read()
 
 
 
@@ -31,9 +20,9 @@ def extract_accel_tar(a_tar):
             ]
         tar.extractall(path="extracted", members=subdir_and_files)
 
+#extract files
 #path is currently set to csv_files
 def extract_from_path():
-
     path = r'csv_files'
 
     for dir_entry in os.listdir(path):
@@ -41,30 +30,13 @@ def extract_from_path():
         extract_accel_tar(my_tar)
 
 
-#look in each tar file
-# path = r'/csv_files'
-# data = {}
-# for dir_entry in os.listdir(path):
-#     dir_entry_path = os.path.join(path, dir_entry)
-#     with open(dir_entry_path, 'r') as my_file:
-#         data[dir_entry] = my_file.read()
+def parse_lines(lines_in_file):
+    with open("master_hourly.csv", 'ab') as master_file:
+        master_file.write(lines_in_file)
 
-
-
-
-# with open('output.txt','w') as fout:
-#     for root, subFolders, files in os.walk(rootdir):
-#         if 'data.txt' in files:
-#             with open(os.path.join(root, 'data.txt'), 'r') as fin:
-#                 for lines in fin:
-#                     dosomething()
-
-
-def parse_lines(text):
-    with open("master_hourly", "ab") as master_file:
-        master_file.write(text)
-
-#ASSIGN ROOTDIR TO OPEN AND PARSE FILES
+#open all files to parse out lines
+#rootdir currently assigned to extracted
+#TO DO: make this more efficient! 
 def open_files():
     rootdir = "extracted"
     for root, subFolders, files in os.walk(rootdir):
@@ -74,14 +46,42 @@ def open_files():
                     parse_lines(lines)
 
 
-open_files()
+#initial RMS calculation
+#Not sure if this should be using the CSV module 
+test_file="extracted/HumDynLog_CHERRY_LGE_LGE_A000002872F302_20120122_080000_20120122_090000/hdl_accel_CHERRY_20120122_080000.csv"
 
+def calculate_RMS(a_file):
+    startTime = datetime.now()
+    x_abs = 0
+    x_1hz = 0
+    x_3hz = 0
+    x_6hz = 0
+    x_10hz = 0
+    length = 0
+
+    with open(a_file,'r') as csv_file:
+        csv_file.next()
+        for line in csv_file:
+            length +=1
+            line = line.split(",")
+            x_abs += float(line[2])
+            x_1hz += float(line[6])
+            x_3hz += float(line[7])
+            x_6hz += float(line[8])
+            x_10hz += float(line[9])
+    x_abs_avg = (x_abs/length)
+    x_1hz_avg = (x_1hz/length)
+    x_3hz_avg = (x_3hz/length)
+    x_6hz_avg = (x_6hz/length)
+    x_10hz_avg = (x_10hz/length)
+    print x_abs_avg, x_1hz_avg, x_3hz_avg, x_6hz_avg, x_10hz_avg
+    print(datetime.now()-startTime)
+
+calculate_RMS(test_file)
 
 # Average absolute.deviation, and PSD at 1, 3, 6, and 10hz for each axis over one hour (or length of file if shorter than one hour) (eg x_abs, x_1hz, x_3hz, x_6hz, x_10hz)
 #take root mean square of the x,y,z values to come up with rms_abs, rms_1, rms_3, rms_6, rms_10
 #append data to master_hourly.CSV
-
-
 
 
 
