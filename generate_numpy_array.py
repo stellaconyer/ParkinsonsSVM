4799a43e209141984f95a14418f9af8fb03a42da
Stella Cotton <stella@stellacotton.com>
2013-11-06 17:53:39 -0800

    Working basic SVM function

--- generate_numpy_array.py
+++ generate_numpy_array.py
@@ -6,37 +6,34 @@ def generate_numpy_array():
 	control_data_array = np.genfromtxt('control_master_hourly.csv', usecols= (0,1,2,3,4), delimiter=',')
 	control_labels_array = np.genfromtxt('control_master_hourly.csv', usecols= (5), delimiter=',')
 
-	print len(control_data_array)
-	# print control_labels_array
-
 	# Parkinsons:
 	parkinsons_data_array = np.genfromtxt('parkinsons_master_hourly.csv', usecols= (0,1,2,3,4), delimiter=',')
 	parkinsons_labels_array = np.genfromtxt('parkinsons_master_hourly.csv', usecols= (5), delimiter=',')
 
-	print len(parkinsons_data_array)
-
-	#Unknown variable
-	variable_data_array = np.genfromtxt('variable_master_hourly.csv', usecols= (0,1,2,3,4), delimiter=',')
-
-	# Master:
+	# Master of control and parkinsons:
 	master_data_array = np.concatenate([control_data_array, parkinsons_data_array])
 	master_labels_array = np.concatenate([control_labels_array, parkinsons_labels_array]) 
 
-	print master_data_array
+	#Unknown variable for testing:
+	variable_data_array = np.genfromtxt('variable_master_hourly.csv', usecols= (0,1,2,3,4), delimiter=',')
 
-	X = master_data_array
+	X_unscaled = master_data_array
 	y = master_labels_array
 
+	#Scale X data for mean at 0
+	X = preprocessing.scale(X_unscaled)
 
+	clf = svm.SVC()
+	clf.fit(X, y)
 
-	X_scaled = preprocessing.scale(X)
+	variable = variable_data_array[150]
 
+	prediction = clf.predict([variable])
+	print prediction
 
-	# print "X", X
-	# print "Y", y
-
-	print "X Scaled", X_scaled
-
+# 	SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0, degree=3,
+# gamma=0.0, kernel='rbf', max_iter=-1, probability=False, random_state=None,
+# shrinking=True, tol=0.001, verbose=False)
 
 generate_numpy_array()
 
