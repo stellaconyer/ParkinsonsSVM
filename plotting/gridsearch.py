c08e807442506997ba2d6b7191b5299fe8cccf11
Stella Cotton <stella@stellacotton.com>
2013-11-12 15:32:32 -0800

    Clean up test files

--- plotting/gridsearch.py
+++
@@ -1,7 +0,0 @@
-from sklearn import svm, grid_search, datasets
-iris = datasets.load_iris()
-parameters = {'kernel':('linear', 'rbf'), 'C':[1, 10]}
-svr = svm.SVC()
-clf = grid_search.GridSearchCV(svr, parameters)
-print clf.fit(iris.data, iris.target)
-print clf.score(iris.data, iris.target)
